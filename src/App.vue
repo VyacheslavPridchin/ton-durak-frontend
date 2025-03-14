2a8951f1a6adc92f7b814363e14a8c2614d4beac
GoofyGoof <39170099+VyacheslavPridchin@users.noreply.github.com>
2025-03-14 20:20:28 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -18,52 +18,10 @@ import FullscreenLayout from "@/layouts/FullscreenLayout.vue";
 import SliderLayout from "@/layouts/slider/SliderLayout.vue";
 import Footer from "@/layouts/FooterLayout.vue";
 import NotificationLayout from "@/layouts/NotificationLayout.vue";
-import apiService from "@/services/ApiService.ts";
-import type { AuthRequest, ApiResponse, AuthResponseData } from "@/services/ApiService.ts";
 
 const router = useRouter();
 const route = useRoute();
 const roots = ref<string[]>(["/referrals", "/main", "/profile"]);
-
-const authData = ref<ApiResponse<AuthResponseData> | null>(null);
-
-// Функция авторизации
-const auth = async (init_data: string) => {
-  const payload: AuthRequest = { initData: init_data };
-  try {
-    authData.value = await apiService.auth(payload);
-    console.log("Auth Response:", authData.value);
-  } catch (error) {
-    console.error("Authorization error:", error);
-  }
-  return { authData };
-};
-
-// Функция кэширования initData и данных пользователя
-const cacheUserData = (rawInitData: string) => {
-  if (rawInitData) {
-    // Сохраняем полную строку initData для будущих обращений
-    localStorage.setItem("initData", rawInitData);
-  }
-  const params = new URLSearchParams(rawInitData);
-  const userEncoded = params.get("user");
-  if (userEncoded) {
-    try {
-      const userJson = decodeURIComponent(userEncoded);
-      const userObj = JSON.parse(userJson);
-      // Сохраняем данные пользователя в кэше
-      localStorage.setItem("userData", JSON.stringify(userObj));
-      // Также можно положить их в глобальную переменную для быстрого доступа
-      // @ts-ignore
-      window.userData = userObj;
-      console.log("Cached user data:", userObj);
-    } catch (err) {
-      console.error("Error parsing user data", err);
-    }
-  } else {
-    console.warn("User data not found in initData");
-  }
-};
 
 onMounted(async () => {
   // @ts-ignore
@@ -83,16 +41,6 @@ onMounted(async () => {
 
   // @ts-ignore
   window.Telegram.WebApp.lockOrientation();
-
-  // Пытаемся получить initData из Telegram.WebApp или из кэша
-  // @ts-ignore
-  const telegramInitData = window.Telegram.WebApp.initData || localStorage.getItem("initData");
-  if (telegramInitData) {
-    cacheUserData(telegramInitData);
-    await auth(telegramInitData);
-  } else {
-    console.error("initData не найден ни в Telegram.WebApp, ни в кэше");
-  }
 
   router.afterEach(() => {
     const idx = window.history.state ? window.history.state.position : 0;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,63 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import {createApp, ref} from 'vue'
 import App from './App.vue'
 import router from './router'
+import type {ApiResponse, AuthRequest, AuthResponseData} from "@/services/ApiService.ts";
+import apiService from "@/services/ApiService.ts";
 
 // @ts-ignore
 const colorScheme = window.Telegram?.WebApp?.colorScheme || 'light'; // По умолчанию 'light'
 document.documentElement.setAttribute('data-theme', colorScheme);
+
+const authData = ref<ApiResponse<AuthResponseData> | null>(null);
+
+// Функция авторизации
+const auth = async (init_data: string) => {
+    const payload: AuthRequest = { initData: init_data };
+    try {
+        authData.value = await apiService.auth(payload);
+        console.log("Auth Response:", authData.value);
+    } catch (error) {
+        console.error("Authorization error:", error);
+    }
+    return { authData };
+};
+
+// Функция кэширования initData и данных пользователя
+const cacheUserData = (rawInitData: string) => {
+    if (rawInitData) {
+        // Сохраняем полную строку initData для будущих обращений
+        localStorage.setItem("initData", rawInitData);
+    }
+    const params = new URLSearchParams(rawInitData);
+    const userEncoded = params.get("user");
+    if (userEncoded) {
+        try {
+            const userJson = decodeURIComponent(userEncoded);
+            const userObj = JSON.parse(userJson);
+            // Сохраняем данные пользователя в кэше
+            localStorage.setItem("userData", JSON.stringify(userObj));
+            // Также можно положить их в глобальную переменную для быстрого доступа
+            // @ts-ignore
+            window.userData = userObj;
+            console.log("Cached user data:", userObj);
+        } catch (err) {
+            console.error("Error parsing user data", err);
+        }
+    } else {
+        console.warn("User data not found in initData");
+    }
+};
+
+// @ts-ignore
+const telegramInitData = window.Telegram.WebApp.initData || localStorage.getItem("initData");
+if (telegramInitData) {
+    cacheUserData(telegramInitData);
+    await auth(telegramInitData);
+} else {
+    console.error("initData не найден ни в Telegram.WebApp, ни в кэше");
+}
 
 const app = createApp(App)
 

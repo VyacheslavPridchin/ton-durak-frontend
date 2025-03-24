861e81ccaf0fceffac9a2e2fd9c95c8e533b3f37
GoofyGoof <39170099+VyacheslavPridchin@users.noreply.github.com>
2025-03-24 18:57:25 +0300

    upd

--- src/views/pages/MainPage.vue
+++ src/views/pages/MainPage.vue
@@ -121,7 +121,6 @@ export default defineComponent({
       tournamentDeadline,
       walletBalance,
       walletBonus,
-      playAmount,
       leaguePanelRef,
       tournamentPanelRef,
       walletPanelRef,
